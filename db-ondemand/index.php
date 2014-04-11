ea102e7bb014f60f6bc3204f1c696d35929a6a71
mind-v <mind.vodniki@gmail.com>
2014-04-11 18:07:35 +0600

    +checkbox -tables

--- db-ondemand/index.php
+++ db-ondemand/index.php
@@ -1,32 +1,26 @@
-<h2>
-<a href="/dump/">DUMPs</a>
+<a href="/dump/"><h2>DUMPs</h2></a>
 <p></p>
 <center>
-<table>
-<tr>
-<td>
 <form action="" method="post">
-	Databases only:<br>
-	<input type=text name="args" value="<?php echo isset($_POST['args']) ? $_POST['args'] : ''; ?>"/>
-	<button type="submit">Run</button>
+	<input type=text 
+	name="args" 
+	value="<?php echo isset($_POST['args']) ? $_POST['args'] : ''; ?>"/>
+	<button type="submit">Run</button><br>
+	<input type=checkbox  
+	name="dbonly"  
+	value="dbonly" <?php echo isset($_POST['dbonly']) ? "checked" : ''; ?> > Databases only<br>
 </form>
-</td>
-<td width="20%">
-</td>
-<td>
-<form action="" method="post">
-	Database + tables:<br>
-	<input type=text name="args" value="<?php echo isset($_POST['args']) ? $_POST['args'] : ''; ?>"/>
-	<button type="submit">Run</button>
-</form>
-</td>
-</tr>
+<h2>
 <?php
 $cmd = '/usr/bin/db-ondemand';
-$again = '<button onclick="window.location.reload()">Try again</button>';
+if(defined($_POST['dbonly']))
+{
+
+}
 if (isset($_POST['args'])) {
  $args = escapeshellcmd ($_POST['args']);
  $cmd_to_exec = $cmd.' '.$args;
+  var_dump ($dbonly);
  exec("$cmd_to_exec 2>&1", $out);
    foreach($out as $line) {
 		echo $line."<br>";
