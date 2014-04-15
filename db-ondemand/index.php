a9a587ac8ddce0cd9e8157f91742a6bddea88c76
mind-v <mind.vodniki@gmail.com>
2014-04-15 10:44:33 +0600

    added nolimit option, radio buttons

--- db-ondemand/index.php
+++ db-ondemand/index.php
@@ -1,20 +1,40 @@
 <a href="/dump/"><h2>DUMPs</h2></a>
 <p></p>
 <center>
-<form action="" method="post">
+<table>
+<form action="" method="post"><tr><td>
 	<input type=text 
+	size="80"
 	name="args" 
 	value="<?php echo isset($_POST['args']) ? $_POST['args'] : ''; ?>"/>
-	<button type="submit">Run</button><br>
-	<input type=checkbox  
-	name="dbonly"  
-	value="dbonly" <?php echo isset($_POST['dbonly']) ? "checked" : ''; ?> > Databases only<br>
+	<button type="submit">Run</button><br></tr><tr>
+	<td align=left>
+	
+	<input type=radio  
+	name="options"  
+	checked="checked"
+	value="nooptions" <?php echo (isset($_POST['options']) && $_POST['options'] == '') ? "checked" : ''; ?> >
+	 No options
+	<br>
+	
+	<input type=radio  
+	name="options"  
+	value="dbonly" <?php echo (isset($_POST['options']) && $_POST['options'] == 'dbonly') ? "checked" : ''; ?> >
+	 Воспринимать введенное как имена баз
+	<br>
+	<input type=radio  
+	name="options"  
+	value="nolimit" <?php echo (isset($_POST['options']) && $_POST['options'] == 'nolimit') ? "checked" : ''; ?> >
+	 Disable "LIMIT 10000"</td></tr>
 </form>
+</table>
 <h2>
-<?php
 
-if(isset($_POST['dbonly'])) {
+<?php
+if(isset($_POST['options']) && $_POST['options'] == 'dbonly') {
 	$cmd = '/usr/bin/db-ondemand dbonly';
+} elseif(isset($_POST['options']) && $_POST['options'] == 'nolimit') {
+	$cmd = '/usr/bin/db-ondemand nolimit';
 } else {
 	$cmd = '/usr/bin/db-ondemand';
 }
@@ -22,6 +42,7 @@ if(isset($_POST['dbonly'])) {
 if (isset($_POST['args'])) {
  	$args = escapeshellcmd ($_POST['args']);
  	$cmd_to_exec = $cmd.' '.$args;
+	echo "<!-- $cmd_to_exec.<br -->";
 	exec("$cmd_to_exec 2>&1", $out);
 	foreach($out as $line) {
 		echo $line."<br>";
